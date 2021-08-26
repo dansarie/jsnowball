6bb0b3629a5c5e6f581732483933ee82658b6401
Marcus Dansarie <marcus@dansarie.se>
2021-08-26 09:59:33 +0200

    Better graph drawing. Fixes #4.

--- app/src/main/java/se/dansarie/jsnowball/gui/GraphPanel.java
+++ app/src/main/java/se/dansarie/jsnowball/gui/GraphPanel.java
@@ -11,6 +11,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -37,8 +38,8 @@ public abstract class GraphPanel<E> extends JPanel implements ListDataListener {
   private Map<E, Node<E>> memberMap = new HashMap<>();
   private Node<E> draggedNode = null;
   private Set<GraphListener<E>> listeners = new HashSet<>();
-  private float temp = 20;
-  private Timer timer = new Timer(50, ev -> redrawGraphs());
+  private float temp = 30;
+  private Timer timer = new Timer(50, ev -> redrawGraph());
   private Predicate<E> filter = m -> true;
 
   public GraphPanel() {
@@ -82,7 +83,7 @@ public abstract class GraphPanel<E> extends JPanel implements ListDataListener {
     nodeMap.clear();
     memberMap.clear();
     draggedNode = null;
-    temp = 20;
+    temp = 30;
     getListModel().addListDataListener(this);
   }
 
@@ -130,6 +131,9 @@ public abstract class GraphPanel<E> extends JPanel implements ListDataListener {
       nodeMap.put(i, node);
     }
     memberMap.values().retainAll(nodeMap.values());
+    if (temp < 10) {
+      temp = 10;
+    }
     updateEdges();
     updateSize();
     repaint();
@@ -202,21 +206,31 @@ public abstract class GraphPanel<E> extends JPanel implements ListDataListener {
     n1.addForce(dx, dy);
   }
 
-  private void drawGraph(List<Node<E>> graph, float k) {
+  private void drawGraph(Collection<Node<E>> graph, float k) {
     for (Node<E> n1 : graph) {
       n1.clearForces();
     }
+    float centerx = 0;
+    float centery = 0;
     for (Node<E> n1 : graph) {
       for (Node<E> n2 : graph) {
         if (n1 == n2) {
           continue;
         }
-        updateForce(n1, n2, x -> k * k / x);
+        updateForce(n1, n2, x -> k * k / x); /* Repulsive force. */
       }
       for (Node<E> n2 : n1.getEdges()) {
-        updateForce(n1, n2, x -> -x * x / k);
+        updateForce(n1, n2, x -> -x * x / k); /* Attractive force. */
         updateForce(n2, n1, x -> -x * x / k);
       }
+      centerx += n1.getX();
+      centery += n1.getY();
+    }
+    Node<E> virtualCenter = new Node<>(null);
+    virtualCenter.setX(centerx / graph.size());
+    virtualCenter.setY(centery / graph.size());
+    for (Node<E> n1 : graph) {
+      updateForce(n1, virtualCenter, x -> -x * x / k / 5);
     }
 
     float minx = Integer.MAX_VALUE;
@@ -238,101 +252,21 @@ public abstract class GraphPanel<E> extends JPanel implements ListDataListener {
         node.setY(y);
       }
     }
-    if (minx < 0 || miny < 0) {
-      for (Node <E> node : graph) {
-        if (minx < 0) {
-          node.setX(node.getX() - minx);
-        }
-        if (miny < 0) {
-          node.setY(node.getY() - miny);
-        }
-      }
+    for (Node <E> node : graph) {
+      node.setX(node.getX() - minx);
+      node.setY(node.getY() - miny);
     }
   }
 
-  private Rectangle getGraphBounds(List<Node<E>> graph) {
-    Rectangle bounds = graph.get(0).getShape().getBounds();
-    for (Node<E> node : graph) {
-      bounds = bounds.union(node.getShape().getBounds());
+  private void redrawGraph() {
+    if (temp < 0.001) {
+      return;
     }
-    return bounds;
-  }
+    drawGraph(nodeMap.values(), 60);
+    temp *= 0.97;
 
-  private void redrawGraphs() {
-    List<List<Node<E>>> graphs = getGraphs();
-    Collections.sort(graphs, (l1, l2) -> l2.size() - l1.size());
-
-    for (List<Node<E>> graph : graphs) {
-      drawGraph(graph, 60);
-    }
-    if (temp > 0.2) {
-      temp *= 0.99;
-    }
-
-    for (List<Node<E>> graph : graphs) {
-      Rectangle bounds = getGraphBounds(graph);
-      for (Node<E> node : graph) {
-        if (node != draggedNode) {
-          node.setX(node.getX() - bounds.x);
-          node.setY(node.getY() - bounds.y);
-        }
-      }
-    }
-
-    Rectangle bounds = null;
-    for (List<Node<E>> graph : graphs) {
-      Rectangle graphBounds = getGraphBounds(graph);
-      /* Check if graph intersects with previous graphs. */
-      if (bounds != null && graphBounds.intersects(bounds)) {
-        float dx = 0;
-        float dy = 0;
-        if (bounds.x + bounds.width + graphBounds.width <
-            2 * (bounds.y + bounds.height + graphBounds.height)) {
-          dx = bounds.x + bounds.width  - graphBounds.x;
-        } else {
-          dy = bounds.y + bounds.height - graphBounds.y;
-        }
-        for (Node<E> node : graph) {
-          if (node != draggedNode) {
-            node.setX(node.getX() + dx);
-            node.setY(node.getY() + dy);
-          }
-        }
-        graphBounds = getGraphBounds(graph);
-      }
-
-      /* Update bounds of previous graphs with current. */
-      if (bounds == null) {
-        bounds = graphBounds;
-      } else {
-        bounds = bounds.union(graphBounds);
-      }
-    }
     updateSize();
     repaint();
-  }
-
-  private List<List<Node<E>>> getGraphs() {
-    for (Node<E> no : nodeMap.values()) {
-      no.graphnum = -1;
-    }
-    int nextnum = 0;
-    for (Node<E> no : nodeMap.values()) {
-      if (no.graphnum != -1) {
-        continue;
-      }
-      no.setGraphnum(nextnum++);
-    }
-
-    ArrayList<List<Node<E>>> graphs = new ArrayList<>();
-    for (int i = 0; i < nextnum; i++) {
-      graphs.add(new ArrayList<>());
-    }
-    for (Node<E> no : nodeMap.values()) {
-      graphs.get(no.graphnum).add(no);
-    }
-
-    return graphs;
   }
 
   private class GPMouseListener extends MouseAdapter {
@@ -346,7 +280,7 @@ public abstract class GraphPanel<E> extends JPanel implements ListDataListener {
           return;
         }
       }
-      redrawGraphs();
+      redrawGraph();
     }
 
     @Override
@@ -356,6 +290,9 @@ public abstract class GraphPanel<E> extends JPanel implements ListDataListener {
       }
       draggedNode.setX(ev.getX());
       draggedNode.setY(ev.getY());
+      if (temp < 2) {
+        temp = 2;
+      }
       updateSize();
       repaint();
     }
@@ -381,14 +318,13 @@ public abstract class GraphPanel<E> extends JPanel implements ListDataListener {
     private List<Node<E>> edges = new ArrayList<>();
     private List<Node<E>> edgesTo = new ArrayList<>();
     private Ellipse2D.Float shape = new Ellipse2D.Float(0, 0, 25, 25);
-    private int graphnum = -1;
     float force_x = 0;
     float force_y = 0;
     float remain_x = 0;
     float remain_y = 0;
 
     private Node(E member) {
-      this.member = Objects.requireNonNull(member);
+      this.member = member;
     }
 
     private E getMember() {
@@ -455,24 +391,6 @@ public abstract class GraphPanel<E> extends JPanel implements ListDataListener {
       y -= remain_y;
       shape.y = (int)y;
       remain_y = shape.y - y;
-    }
-
-    private void setGraphnum(int num) {
-      graphnum = num;
-      for (Node<E> no : edges) {
-        if (no.graphnum == -1) {
-          no.setGraphnum(num);
-        } else if (no.graphnum != num) {
-          throw new IllegalStateException();
-        }
-      }
-      for (Node<E> no : edgesTo) {
-        if (no.graphnum == -1) {
-          no.setGraphnum(num);
-        } else if (no.graphnum != num) {
-          throw new IllegalStateException();
-        }
-      }
     }
 
     @Override
