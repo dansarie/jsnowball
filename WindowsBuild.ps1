d6a1e2ae610989b30d97da3adf2acebbd3aaa46d
Marcus Dansarie <marcus@dansarie.se>
2021-11-26 18:03:49 +0100

    Add copyright headers.

--- WindowsBuild.ps1
+++ WindowsBuild.ps1
@@ -1,3 +1,17 @@
+# Copyright (c) 2021 Marcus Dansarie
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 Start-BitsTransfer -Source https://aka.ms/download-jdk/microsoft-jdk-17-windows-x64.zip, https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip
 Expand-Archive -Path microsoft-jdk-17-windows-x64.zip
 Expand-Archive -Path wix311-binaries.zip
